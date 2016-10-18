4947e7eeb36a27a7daec29980c54a1438afae49f
navneetagarwal <navneet@cse.iitb.ac.in>
2016-10-18 19:20:17 +0530

    Added for reader

--- InsertRelation.sql
+++ InsertRelation.sql
@@ -1,3 +1,6 @@
-insert into reader values();
-insert into author values();
+insert into reader values('navneetagarwal','navneet','1995-10-27','Kolkata','navneetagarwal4@gmail.com',null,null,'City of joy resident blah blah blah blah','Male','newuser');
+insert into reader values('tanmayparekh','tanmay','1997-10-04','Ranchi','tparekh97@gmail.com',null,null,'Fraud blah blah blah blah','Male','newuser');
+insert into reader values('ritwickchaudhry','ritwick','1995-10-13','Chandigarh','ritwickchaudhry@gmail.com',null,null,'Football blah blah blah blah','Male','newuser');
+insert into reader values('saurabhgarg','saurabh','1995-11-20','Ahmedabad','garg.saurabh.2014@gmail.com',null,null,'Work hard blah blah blah blah','Male','newuser');
 insert into book values()
+insert into author values();
