73dbc6ae1adf67b0fc8640b7098b598b989418c4
Navneet Agarwal <navneet@cse.iitb.ac.in>
2016-11-19 23:35:25 +0530

    Changed UploadBook get method for getting correct data

--- Servlet_Files/UploadBookPhoto.java
+++ Servlet_Files/UploadBookPhoto.java
@@ -1,3 +1,4 @@
+		
 
 
 import java.io.IOException;
@@ -17,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 /**
- * Servlet implementation class UploadPhoto
+ * Servlet implementation class UploadBookPhoto
  */
 @WebServlet("/UploadBookPhoto")
 @MultipartConfig(maxFileSize = 16177215)
@@ -37,10 +38,11 @@ public class UploadBookPhoto extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
 		String connString = ServletInfo.connString;
 		String username = ServletInfo.userName;
 		String password = ServletInfo.passWord;
-		System.out.println("asdasd");
+		System.out.println("bookphoto");
 
 		try ( 
 				// Trying to connect
@@ -51,7 +53,8 @@ public class UploadBookPhoto extends HttpServlet {
 			response.setContentType("image/jpeg");
 			String bookID = request.getParameter("id");
 			PreparedStatement ps = conn.prepareStatement("SELECT Cover_pic_img FROM book WHERE Book_ID = ?");
-			ps.setString(1, id);
+//			bookID = "4";
+			ps.setString(1, bookID);
 			ResultSet rs = ps.executeQuery();
 			byte[] imgBytes = null;
 			if (rs != null) {
@@ -82,7 +85,7 @@ public class UploadBookPhoto extends HttpServlet {
 		String connString = ServletInfo.connString;
 		String username = ServletInfo.userName;
 		String password = ServletInfo.passWord;
-		System.out.println("asdasd");
+		System.out.println("bookphotopost");
 
 		try ( 
 				// Trying to connect
@@ -90,7 +93,8 @@ public class UploadBookPhoto extends HttpServlet {
 				Statement stmt = conn.createStatement();
 			)
 			{
-			String id = request.getParameter("id");
+//			String id = request.getParameter("id");
+//			System.out.println(id);
 			Part filePart = request.getPart("photo1");
 			InputStream inputStream = null;
 			if (filePart != null) {
@@ -102,15 +106,15 @@ public class UploadBookPhoto extends HttpServlet {
 	            // obtains input stream of the upload file
 	            inputStream = filePart.getInputStream();
 	            System.out.println(inputStream.toString());
-	            String query = "Update reader set profile_picture_name=?,profile_picture_img=? where reader_id=?";
+	            String query = "Update Book set Cover_Pic_name=?,Cover_Pic_img=? where Book_ID=?";
 	            PreparedStatement pstmt = conn.prepareStatement(query);
 	            pstmt.setString(1, filePart.getName());
-	            pstmt.setString(3,id);
+	            pstmt.setString(3,"3");
 	            pstmt.setBinaryStream(2,inputStream,filePart.getSize());
 	            pstmt.executeUpdate();      
 	        }
 			else{
-				System.out.println("asdasd");
+				System.out.println("error");
 			}
 			}
 		catch(Exception e){
